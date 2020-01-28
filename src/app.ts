95439a72b5c1992adbe68ff13d17f52e11b2cb8c
ririko <ririmohu25@gmail.com>
2020-01-29 02:58:05 +0900

    修正

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,14 @@ const server = app.listen(port, () => console.log(`ポート番号は ${port}だ
 
 // ↓ここからSocketIOの処理
 const io = socketIO(server);
+type BoardData = { board: string; lastPlay: number | null };
+
+let currentBoard: BoardData = { board: "---------", lastPlay: null };
 
 io.on("connection", function(socket) {
   console.log(`ユーザーが繋いできた[id:${socket.id}]`);
 
-  socket.emit("CHAT_MESSAGE", { name: "system", message: "helloかに" });
+  // socket.emit("CHAT_MESSAGE", { name: "system", message: "helloかに" });
 
   socket.on("POST_MESSAGE", function(data) {
     console.log(`posted[name:${data.name},message:${data.message}]`);
@@ -22,7 +25,17 @@ io.on("connection", function(socket) {
     io.emit("CHAT_MESSAGE", data);
   });
 
-  socket.on("BOARD", function(data: { board: string }) {
+  socket.on("BOARD", function(data: BoardData) {
+    console.log(data);
+    currentBoard = data;
     io.emit("BOARD", data);
   });
+
+  socket.on("REQUEST_BOARD", function() {
+    socket.emit("BOARD", currentBoard);
+  });
 });
+
+// setInterval(function() {
+//   io.emit("CHAT_MESSAGE", { name: "system", message: Date.now().toString() });
+// }, 1000);
