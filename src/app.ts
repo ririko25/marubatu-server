4b7b420c7d38ad2ae595e49d47463b9ba08a83dc
yoskeoka <yoske95@gmail.com>
2020-08-17 10:58:11 +0900

    in memory chat message history

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as socketIO from "socket.io";
+import Message from "./message";
+import State from "./state";
 
 const app = express();
 const port = 3333;
@@ -10,32 +12,52 @@ const server = app.listen(port, () => console.log(`ポート番号は ${port}だ
 
 // ↓ここからSocketIOの処理
 const io = socketIO(server);
+
+const state = new State();
 type BoardData = { board: string; lastPlay: number | null };
 
 let currentBoard: BoardData = { board: "---------", lastPlay: null };
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   console.log(`ユーザーが繋いできた[id:${socket.id}]`);
 
-  // socket.emit("CHAT_MESSAGE", { name: "system", message: "helloかに" });
-
-  socket.on("POST_MESSAGE", function(data) {
-    console.log(`posted[name:${data.name},message:${data.message}]`);
-
-    io.emit("CHAT_MESSAGE", data);
+  socket.on("disconnecting", (reason) => {
+    console.log(`ユーザーが切断した[id:${socket.id}, reason:${reason}]`);
+    state.removeUser(socket.id);
   });
 
-  socket.on("BOARD", function(data: BoardData) {
+  socket.on("JOIN_ROOM", function (data: { team: string }) {
+    socket.join(data.team);
+    state.addUser(socket.id, data.team);
+  });
+
+  socket.on("LEAVE_ROOM", function () {
+    socket.leaveAll();
+    state.removeUser(socket.id);
+  });
+
+  socket.on("GET_ALL_MESSAGES", function () {
+    const team = state.users.get(socket.id);
+    socket.emit("ALL_MESSAGES", state.getTeamMessages(team));
+  });
+
+  socket.on("POST_MESSAGE", function (msg: Message) {
+    console.log(`posted[user:${msg.user}, team:${msg.team}, text:${msg.text}]`);
+
+    state.addMessage({ id: socket.id, ...msg });
+
+    // for team function
+    // io.in(msg.team).emit("CHAT_MESSAGE", msg);
+    io.emit("CHAT_MESSAGE", msg);
+  });
+
+  socket.on("BOARD", function (data: BoardData) {
     console.log(data);
     currentBoard = data;
     io.emit("BOARD", data);
   });
 
-  socket.on("REQUEST_BOARD", function() {
+  socket.on("REQUEST_BOARD", function () {
     socket.emit("BOARD", currentBoard);
   });
 });
-
-// setInterval(function() {
-//   io.emit("CHAT_MESSAGE", { name: "system", message: Date.now().toString() });
-// }, 1000);
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const Config = {
+  messageHistoryLimit: 20,
+};
+
+export default Config;
